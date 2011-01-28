6e3b019ce12fa8d3f0a72af75365366ad9bc51c8
david <david@b282a14c-f844-f624-09bc-e1946d22bf1e>
2011-01-28 04:19:10 +0000

    deleted Release folder from repo

--- Basestation/Release/Core/WMath.d
+++
@@ -1,4 +0,0 @@
-Core/WMath.d Core/WMath.o: ../Core/WMath.cpp \
-  /Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/avr/include/stdlib.h
-
-/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/avr/include/stdlib.h:
