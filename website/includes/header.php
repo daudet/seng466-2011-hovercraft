aea9d9a51bb81812678606d6ed8980ed553e62b9
david <david@b282a14c-f844-f624-09bc-e1946d22bf1e>
2011-03-07 18:55:07 +0000

    fixed header.php

--- website/includes/header.php
+++ website/includes/header.php
@@ -11,7 +11,7 @@
 	<div id="menu">
 		<ul>
 		<li class="current_page_item"><a href="../index.html">Homepage</a></li>
-		<li><a href="project1.html">Project 1</a></li>
+		<li><a href="../project_1/project1.html">Project 1</a></li>
 		<li><a href="../project_2/project2.html">Project 2</a></li>
 		<li><a href="../project_3/project3.html">Project 3</a></li>
 		<li><a href="../contact.html">Contact</a></li>
