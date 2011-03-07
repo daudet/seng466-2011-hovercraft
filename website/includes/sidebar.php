70641b36016626dedf378beafec59de3147e1697
david <david@b282a14c-f844-f624-09bc-e1946d22bf1e>
2011-03-07 05:06:59 +0000

    added includes folder for website with php files for header and sidebar

---
+++ website/includes/sidebar.php
@@ -0,0 +1,10 @@
+<div id="sidebar">
+		<h2>Project 1</h2>
+		<a href="project1.html">Overview</a><br />
+		<a href="software.html">Software Environment</a><br />
+		<a href="hardware.html">Electronic Components</a><br />
+		<a href="communications.html">Communications</a><br />
+		<a href="gamepad.html">USB PS2 Gamepad</a><br />
+		<a href="physical.html">Physical Layout</a><br />
+		<a href="discussion.html">Discussion</a><br />
+</div>
