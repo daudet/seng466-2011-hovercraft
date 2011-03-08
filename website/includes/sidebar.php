89481137d5f96a206015909251ebe69828af386e
david <david@b282a14c-f844-f624-09bc-e1946d22bf1e>
2011-03-08 05:01:42 +0000

    added skirt.html and platform.html to project 2 website

--- website/includes/sidebar.php
+++ website/includes/sidebar.php
@@ -7,15 +7,4 @@
 		<a href="gamepad.html">USB PS2 Gamepad</a><br />
 		<a href="physical.html">Physical Layout</a><br />
 		<a href="discussion.html">Discussion</a><br />
-
-		<h2>Project 2</h2>
-		<a href="project2.html">Overview</a><br />
-		<a href="">Platform Design</a><br />
-		<a href="">Skirt Design</a><br />
-		<a href="">Lifting Motor Drivers</a><br />
-		<a href="">Motor Dead-Zone Mapping</a><br />
-		<a href="">Wii Controller</a><br />
-		<a href="">Instrumentation</a><br />
-
-		
 </div>
---
+++ website/project_2/sidebar.php
@@ -0,0 +1,12 @@
+<div id="sidebar">
+
+		<h2>Project 2</h2>
+		<a href="project2.html">Overview</a><br />
+		<a href="platform.html">Platform Design</a><br />
+		<a href="skirt.html">Skirt Design</a><br />
+		<a href="">Lifting Motor Drivers</a><br />
+		<a href="">Motor Dead-Zone Mapping</a><br />
+		<a href="">Wii Controller</a><br />
+		<a href="">Instrumentation</a><br />
+
+</div>
